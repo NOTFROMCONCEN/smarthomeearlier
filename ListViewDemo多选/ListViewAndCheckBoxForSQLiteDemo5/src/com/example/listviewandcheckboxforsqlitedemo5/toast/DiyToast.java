70cad8d0c209300ee777728a3e830ebfd4543dfe
大白兔奶糖 <1097681347@qq.com>
2019-10-17 10:40:32 +0800

    ListView多选

---
+++ ListViewDemo多选/ListViewAndCheckBoxForSQLiteDemo5/src/com/example/listviewandcheckboxforsqlitedemo5/toast/DiyToast.java
@@ -0,0 +1,25 @@
+package com.example.listviewandcheckboxforsqlitedemo5.toast;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * @author Administrator
+ * @year 2019
+ * @Todo TODO ×Ô¶¨ÒåToast
+ * @package_name com.example.listviewandcheckboxforsqlitedemo5.toast
+ * @project_name ListViewAndCheckBoxForSQLiteDemo5
+ * @file_name DiyToast.java
+ */
+public class DiyToast {
+	public static Toast toast;
+
+	public static void showToast(Context context, String string) {
+		if (toast == null) {
+			toast = Toast.makeText(context, string, Toast.LENGTH_SHORT);
+		} else {
+			toast.setText(string);
+		}
+		toast.show();
+	}
+}
